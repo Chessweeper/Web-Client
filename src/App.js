18971869bd06833db5f59c72346744f0fe923f42
Xwilarg <xwilarg@protonmail.com>
2022-11-13 20:04:26 +0100

    Improve display

--- src/App.js
+++ src/App.js
@@ -63,11 +63,36 @@ class App {
                 const id = cell.dataset.id;
                 if (!this.state.G.knownCells[id]) {
                     if (Number.isInteger(this.state.G.cells[id])) {
+                        /* To discover many pieces at once, doesn't really work well game design wise
                         if (this.state.G.cells[id] === 0) {
                             tryDiscover(this.client, this.state.G, this.rootElement, Math.floor(id / 8), id % 8, []);
-                        } else {
+                        } else */
+                        {
                             this.client.moves.discoverPiece(id);
                             cell.classList.add("open");
+
+                            // Board color
+                            const y = Math.floor(id / 8);
+                            const x = id % 8;
+                            const isWhite = (y % 2 == 0 && x % 2 == 0) || (y % 2 == 1 && x % 2 == 1)
+                            cell.classList.add(isWhite ? "white" : "black");
+
+                            // Text color
+                            const colors = [
+                                "#0001FD", // 1
+                                "#017E00", // 2
+                                "#FE0000", // 3
+                                "#010082", // 4
+                                "#830003", // 5
+                                "#008080", // 6
+                                "#000000", // 7
+                                "#808080", // 8
+                            ];
+                            let color = "";
+                            if (this.state.G.cells[id] === 0) color = "";
+                            else if (this.state.G.cells[id] > 8) color = colors[7];
+                            else color = colors[this.state.G.cells[id] - 1];
+                            cell.style = "color: " + color + ";";
                         }
                     } else {
                         console.log("You lost");
