74cd1ee38072022d7703b9a0ee773d71ba24d81d
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-11-28 16:04:57 -0600

    finish timer tests

--- test/components/Timer.test.tsx
+++ test/components/Timer.test.tsx
@@ -1,26 +1,44 @@
 // @vitest-environment jsdom
-import userEvent from "@testing-library/user-event";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import {
   BoardContext,
   BoardContextState,
 } from "../../src/components/BoardWrapper";
-import { Timer } from "../../src/components/Timer";
+import { Timer, TimerRefAttributes } from "../../src/components/Timer";
 import { createMockBoardContext } from "../mocks";
-import { createRef } from "react";
+import { createRef, MutableRefObject } from "react";
+import { act } from "react-dom/test-utils";
 
 let boardContext: BoardContextState;
 
 describe("Timer tests", () => {
   beforeEach(() => {
     boardContext = createMockBoardContext();
-    vi.clearAllMocks();
+    vi.useFakeTimers();
   });
 
-  it("should", async () => {
-    const timerRef = createRef<any>();
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
 
-    const { container, rerender } = render(
+  it("should start timer with start imperative handle", async () => {
+    const timerRef = createRef() as MutableRefObject<TimerRefAttributes>;
+
+    render(
+      <BoardContext.Provider value={boardContext}>
+        <Timer ref={timerRef} />
+      </BoardContext.Provider>
+    );
+
+    timerRef.current.start();
+
+    expect(timerRef.current.isRunning()).toBe(true);
+  });
+
+  it("should stop timer on gameover", async () => {
+    const timerRef = createRef() as MutableRefObject<TimerRefAttributes>;
+
+    const { rerender } = render(
       <BoardContext.Provider value={boardContext}>
         <Timer ref={timerRef} />
       </BoardContext.Provider>
@@ -28,7 +46,6 @@ describe("Timer tests", () => {
 
     timerRef.current.start();
     boardContext.ctx.gameover = { isWin: true };
-    // await new Promise((r) => setTimeout(r, 2000));
 
     rerender(
       <BoardContext.Provider value={boardContext}>
@@ -36,8 +53,46 @@ describe("Timer tests", () => {
       </BoardContext.Provider>
     );
 
+    expect(timerRef.current.isRunning()).toBe(false);
+  });
+
+  it("should render time with trailing zero if ms is less than 10", async () => {
+    const timerRef = createRef() as MutableRefObject<TimerRefAttributes>;
+
+    const { container } = render(
+      <BoardContext.Provider value={boardContext}>
+        <Timer ref={timerRef} />
+      </BoardContext.Provider>
+    );
+
+    timerRef.current.start();
+
+    act(() => {
+      vi.advanceTimersByTime(50);
+    });
+
     const timerDiv = container.querySelector("#timer");
 
-    expect.anything();
+    expect(timerDiv?.textContent).toBe("0:05");
+  });
+
+  it("should not render time with trailing zero if ms is at least 10", async () => {
+    const timerRef = createRef() as MutableRefObject<TimerRefAttributes>;
+
+    const { container } = render(
+      <BoardContext.Provider value={boardContext}>
+        <Timer ref={timerRef} />
+      </BoardContext.Provider>
+    );
+
+    timerRef.current.start();
+
+    act(() => {
+      vi.advanceTimersByTime(100);
+    });
+
+    const timerDiv = container.querySelector("#timer");
+
+    expect(timerDiv?.textContent).toBe("0:10");
   });
 });
