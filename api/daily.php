376acb673a1b2543fd2971044fbcaf57bcf162e7
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-12-01 00:34:53 -0600

    use utc tz in api

--- api/daily.php
+++ api/daily.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('UTC');
+
 function generateRandomString() {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
