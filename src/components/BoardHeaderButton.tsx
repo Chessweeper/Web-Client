fd36b5de6fb86b7a5a1bfd2e6fc5d5a7613e15bf
mbrinkl <bjbrinkleyyy@yahoo.com>
2022-12-09 23:34:50 -0600

    board header improvements

--- src/components/BoardHeaderButton.tsx
+++ src/components/BoardHeaderButton.tsx
@@ -2,11 +2,19 @@ import rook from "../assets/wR.png";
 import knook from "../assets/knook.png";
 import pawn from "../assets/wP.png";
 import { useBoardContext } from "./BoardWrapper";
+import { useEffect } from "react";
 
 export const BoardHeaderButton = (): JSX.Element => {
   const { ctx, reload } = useBoardContext();
 
   let image = rook;
+
+  // Preload images
+  useEffect(() => {
+    new Image().src = rook;
+    new Image().src = knook;
+    new Image().src = pawn;
+  }, []);
 
   if (ctx.gameover?.isWin === true) {
     image = knook;
